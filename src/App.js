b1ff90a5f552c271df01f95bb20d2c6875361503
Arnaud Malanda <arnaud.malanda@gmail.com>
2019-03-27 10:07:18 +0100

    [Quest 02][Completed]

--- src/App.js
+++ src/App.js
@@ -12,18 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">My Travel App</h1>
         </header>
-        <Travel
-          destination="Melbourne"
-          country="Australia"
-          distance="16 781"
-          photo="https://www.mantrahotels.com/Portals/0/GalleryImages/System/Locations/Australia/Victoria/Melbourne&Surrounds/Melbourne/Melbourne.t76962.jpg"
-        />
-        <Travel
-          destination="Brazzaville"
-          country="Congo"
-          distance="5 687"
-          photo="https://upload.wikimedia.org/wikipedia/commons/8/89/Brazza_bridge.jpg"
-        />
+        <Travel />
       </div>
     );
   }
--- src/Travel.js
+++ src/Travel.js
@@ -1,11 +1,50 @@
 import React from "react";
 
-const Travel = ({ destination, country, photo, distance }) => (
+const travels = [
+    {
+        destination: "Melbourne",
+        country: "Australia",
+        distance: "16 781",
+        photo: "https://www.mantrahotels.com/Portals/0/GalleryImages/System/Locations/Australia/Victoria/Melbourne&Surrounds/Melbourne/Melbourne.t76962.jpg"
+    },
+    {
+        destination: "Congo",
+        country: "Brazzaville",
+        distance: "5 687",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/8/89/Brazza_bridge.jpg"
+    },
+    {
+        destination: "London",
+        country: "United Kingdom",
+        distance: "343",
+        photo: "https://cdn.londonandpartners.com/assets/73295-640x360-london-skyline-ns.jpg"
+    },
+    {
+        destination: "New York",
+        country: "USA",
+        distance: "5 834",
+        photo: "https://lonelyplanetimages.imgix.net/mastheads/GettyImages-538096543_medium.jpg?sharp=10&vib=20&w=1200"
+    },
+    {
+        destination: "Tokyo",
+        country: "Japan",
+        distance: "9 707",
+        photo: "https://bonjour-france-japon.com/wp-content/uploads/2018/12/tokyo1.jpg"
+    }
+]
+
+const Travel = () => (
     <div>
-        <h1>{destination}, {country}</h1>
-        <h2>{distance} km away</h2>
-        <img src={photo} alt=""/>
+        {travels.map(travel => (
+            <div>
+                <h1>{travel.destination}, {travel.country}</h1>
+                <h2>{travel.distance} km away</h2>
+                <img src={travel.photo} alt="" />
+            </div>
+        ))
+        }
     </div>
+
 );
 
 export default Travel;
